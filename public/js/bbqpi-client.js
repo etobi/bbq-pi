005b2b21471c66d92966aff6c67c37231b6b867f
Tobias Liebig <work@etobi.de>
2017-06-08 02:02:14 +0200

    [TASK] improve interface

--- public/js/bbqpi-client.js
+++ public/js/bbqpi-client.js
@@ -1,14 +1,118 @@
 $(document).ready(function () {
 	var socket = io();
+
+	Vue.filter('formatDateDDMMYYHHMM', function (value) {
+		if (value) {
+			return moment(value).format('DD.MM.YYYY hh:mm')
+		}
+	});
+	Vue.filter('fromNow', function (value) {
+		if (value) {
+			return moment(value).fromNow();
+		}
+	});
+
 	var vm = new Vue({
 		el: '#app',
 		data: {
 			lastUpdate: null,
 			status: '',
 			probes: {},
-			sessionIdentifier: ''
+			session: {},
+			tempchartTimerange: '1h',
+			tempchartAggregation: '1m',
+			tempchart: null
 		},
-		methods: {},
+		methods: {
+			setTempchartTimerange: function (timerange, aggregation) {
+				this.tempchartTimerange = timerange;
+				this.tempchartAggregation = aggregation;
+				this.updateTempchart();
+			},
+			initTempchart: function () {
+				$("<div id='tooltip' class='lead label label-default'></div>").css({
+					position: "absolute",
+					display: "none"
+				}).appendTo("body");
+
+				this.tempchart = $.plot(
+						"#tempchart",
+						[], {
+							grid: {
+								borderColor: "#f3f3f3",
+								borderWidth: 0,
+								tickColor: "#f3f3f3",
+								hoverable: true
+							},
+							series: {
+								shadowSize: 0,
+								color: "#d2d6de",
+								lines: {show: true},
+								points: {show: true}
+							},
+							lines: {
+								fill: true,
+								color: "#d2d6de"
+							},
+							yaxis: {
+								show: true
+							},
+							xaxis: {
+								show: true,
+								mode: 'time',
+								timezone: 'browser'
+							}
+						});
+
+				$('#tempchart').bind("plothover", function (event, pos, item) {
+					if (item) {
+						var x = item.datapoint[0], y = item.datapoint[1].toFixed(2);
+						var time = moment(x).format('hh:mm')
+						$("#tooltip").html(
+								item.series.label + ", " + time + ", " + y + "°C"
+						)
+								.css({top: item.pageY + 5, left: item.pageX + 5})
+								.fadeIn(200);
+					} else {
+						$("#tooltip").hide();
+					}
+				});
+			},
+			updateTempchart: function () {
+				if (this.session.identifier && $('#tempchart').is(':visible')) {
+					var that = this;
+
+					var influxdb = 'http://' + window.location.hostname + ':8086/query';
+
+					var query = "SELECT MEAN(value) FROM temp " +
+								"WHERE \"sessionIdentifier\" = '" + this.session.identifier + "' " +
+								"AND time > now() - " + this.tempchartTimerange +
+								" GROUP BY probe, time(" + this.tempchartAggregation + "),* fill(0)";
+					$.get(influxdb, {
+								db: 'bbqpi',
+								q: query
+							},
+							function (influxData) {
+								if (influxData.results[0].series) {
+									var chartData = influxData.results[0].series.map(function (s) {
+										return {
+											label: that.probes[s.tags.probe].title,
+											color: that.probes[s.tags.probe].color,
+											data: s.values.map(function (v) {
+												return [new Date(v[0]).getTime(), v[1]];
+											})
+										};
+									});
+									that.tempchart.setData(chartData);
+									that.tempchart.setupGrid();
+									that.tempchart.draw();
+								}
+							}
+					);
+				}
+			}
+		},
+
 		created: function () {
 			socket.on('probes-config-update', function (data) {
 				var that = this;
@@ -18,11 +122,16 @@ $(document).ready(function () {
 			}.bind(this));
 
 			socket.on('session-update', function (data) {
-				this.sessionIdentifier = data.sessionIdentifier;
+				this.session = data;
+				this.initTempchart();
+				this.updateTempchart();
+				window.setInterval(function () {
+					this.updateTempchart();
+				}.bind(this), 10000);
 			}.bind(this));
 
 			socket.on('probe-update', function (data) {
-				this.lastUpdate = data.timestamp;
+				this.lastUpdate = new Date(data.timestamp);
 				Vue.set(this.probes[data.channel], 'active', data.active);
 				Vue.set(this.probes[data.channel], 'value', data.value);
 			}.bind(this));
